63f573d1dc84a8e63034fe9d8b9d96eb197dad56
Wifinigel <wifinigel@gmail.com>
2021-12-08 19:59:51 +0000

    add auto generation of all noun combos

--- chatbot/utils/parser.py
+++ chatbot/utils/parser.py
@@ -1,7 +1,11 @@
 import os
 import re
-
 import yaml
+
+# TODO: automate this pull in all verbs and find 
+# all combos. Identify no-noun verbs from commands
+# that contain no "_" character to remove req for
+# static list
 
 # read in the verb definitions from yaml file
 with open(
@@ -41,7 +45,67 @@ def verb_expander(cmd_verb):
     return False
 
 
-def parse_cmd(cmd_text, command_list):
+def noun_combos(command_dict):
+
+    # pull out the nouns from the global command dict
+    command_list = list(command_dict.keys())
+    command_list.sort()
+
+    noun_list = []
+
+    # build list of nouns
+    for command in command_list:
+
+        noun = ''
+
+        # check this isn't a noun-only cmd
+        if "_" in command:
+            cmd_list = command.split("_")
+            noun = cmd_list[1]
+        else:
+            noun = command
+
+        # if unique noun, add to list
+        if noun not in noun_list:
+            noun_list.append(noun)
+    
+    # create a list of all combos for global lookup
+    noun_combos = {}
+
+    # sort the list
+    noun_list.sort(reverse=True)
+
+    for noun in noun_list:
+
+        # put ful command in combo list
+        noun_combos[noun] = noun
+
+        # step through combos, from longest to shortest (min 2 chars)
+        # if already exists, this indicate a duplicate, so flag for
+        # removal
+        for length in range(len(noun)-1, 1, -1):
+
+            new_entry = noun[0:length]
+
+            if new_entry in noun_combos.keys():
+                # already exists, flag for removal
+                noun_combos[new_entry] = "remove_me"
+            else:
+                noun_combos[new_entry] = noun
+
+    # tidy up duplicates    
+
+    # make a copy so we don't break iteration
+    noun_combos_copy = noun_combos.copy()
+    
+    for noun_variant in noun_combos_copy.keys():
+
+        if noun_combos_copy[noun_variant] == "remove_me":
+            del noun_combos[noun_variant]
+    
+    return noun_combos
+
+def parse_cmd(cmd_text, command_list, noun_combos):
 
     """
     Function to parse a string passed to this parser to find a match
@@ -54,7 +118,7 @@ def parse_cmd(cmd_text, command_list):
 
     Verb noun [arg1]...[argX]
 
-    Supported verbs include (check utils/verbs.yml for gull list):
+    Supported verbs include (check utils/verbs.yml for full list):
 
     - exec (unambiguous abbreviations: e, ex, exe)
     - set (unambiguous abbreviations: se)
@@ -68,16 +132,24 @@ def parse_cmd(cmd_text, command_list):
     3. Verify if verb (or provide shortened version) is supported
     4. Expand verb if required
     5. Check if noun is required, ignore noun iteration if not
-    5. Iterate through command string, adding nouns until a command match is achieved
-    6. Return the matched command & remaining tokens as arg list
-    7. If no command match achieved, return False
+    6. Expand noun if required
+    7. Iterate through command strings, adding noun until a command match is achieved
+    8. Return the matched command & remaining tokens as arg list
+    9. If no command match achieved, return False
 
     """
 
     # tokenize & extract noun
     tokens = cmd_text.split()
-    verb = tokens[0]
-    nouns = tokens[1:]
+    
+    # lower case to remove case sensitivity
+    verb = tokens[0].lower()
+    
+    noun =  ''
+    if len(tokens) > 1:
+        # lower case to remove case sensitivity
+        noun = tokens[1].lower()
+    
     args = []
 
     # check verb for possible abbreviation expansion
@@ -93,8 +165,8 @@ def parse_cmd(cmd_text, command_list):
     # this branch deals with a verb only command (e.g. ping)
     if verb in no_noun:
 
-        # if nouns list is zero, we may have a single verb cmd (e.g. speedtest)
-        if len(nouns) == 0:
+        # if noun is empty, we may have a single verb cmd (e.g. speedtest)
+        if noun == "":
             if verb in no_noun:
                 return [cmd, []]
         else:
@@ -109,21 +181,25 @@ def parse_cmd(cmd_text, command_list):
 
     # this branch deals with a verb + noun command (e.g. show cdp)
     else:
+        # substitute in full noun if only noun supplied
+        if noun in noun_combos.keys():
+            noun  =  noun_combos[noun]
+        
         # Iterate through nouns to find command match by adding each token
-        for noun in nouns:
+        #for noun in nouns:
 
-            # check if the verb needs a noun or not
-            cmd = cmd + "_" + noun
-            arg_start += 1
+        # check if the verb needs a noun or not
+        cmd = cmd + "_" + noun
+        arg_start += 1
 
-            if cmd in command_list:
-                # we got a match, slice off args and return the command
+        if cmd in command_list:
+            # we got a match, slice off args and return the command
 
-                for arg in list(tokens[arg_start:]):
-                    args.append(arg)
+            for arg in list(tokens[arg_start:]):
+                args.append(arg)
 
-                # format: [ str, list ]
-                return [cmd, sanitize_args(args)]
+            # format: [ str, list ]
+            return [cmd, sanitize_args(args)]
 
     # no match, return False
     return [False, []]
