82febd819e0dc806139fdfb09cc38e65bd2c706b
Josh Schmelzle <josh@joshschmelzle.com>
2021-12-12 00:10:42 -0500

    bash script is in this package now

--- chatbot/utils/status.py
+++ chatbot/utils/status.py
@@ -2,8 +2,8 @@ import os
 
 
 def get_status():
-
-    stream = os.popen("/opt/wlanpi-chat-bot/etc/scripts/probe_status_msg.sh")
+    """ Read in status from bash script """
+    here = os.path.dirname(os.path.realpath(__file__))
+    stream = os.popen(os.path.join(here, "../bash_scripts/probe_status_msg.sh"))
     startup_msg = stream.read()
-
     return startup_msg
